26f9c7732f9762d1b164a4ab370c2e761ab1c8cd
Ian Heggie (OAF) <ian+oaf@heggie.biz>
2025-02-16 18:02:14 +1100

    Tell me about yourself on latest platform and supported ruby

--- Gemfile
+++ Gemfile
@@ -4,7 +4,27 @@
 
 source "https://rubygems.org"
 
-ruby "2.0.0"
+ruby "3.2.2"
 
-gem "scraperwiki", git: "https://github.com/openaustralia/scraperwiki-ruby.git", branch: "morph_defaults"
-gem "mechanize"
+# Core dependencies
+gem "activesupport", "~> 7.0.8" # Reduced for compatibility
+gem "mechanize", "~> 2.8.5" # Older version for nokogiri compatibility
+gem "nokogiri", "~> 1.15.0" # Compatible version, no platform constraint
+gem "sqlite3", "~> 1.6.3" # Older version that works on heroku-18
+gem "scraperwiki", git: "https://github.com/openaustralia/scraperwiki-ruby.git",
+    branch: "morph_defaults"
+
+# Development and testing dependencies (not used on heroku-18)
+group :development, :test do
+  gem "mini_magick", "~> 4.12"
+  gem "rake", "~> 13.0" # Required update for Ruby 3.2
+  gem "rspec", "~> 3.12" # Latest stable
+  gem "rubocop", "~> 1.57" # Latest with good Ruby 3.x support
+  gem "simplecov", "~> 0.22.0", require: false
+  gem "timecop", "~> 0.9"
+  gem "vcr", "~> 6.2"
+  gem "watir", "~> 7.3" # Latest stable with Selenium 4 support
+  gem "webmock", "~> 3.19"
+end
+
+
--- scraper.rb
+++ scraper.rb
@@ -1,19 +1,50 @@
 # This is a template for a Ruby scraper on morph.io (https://morph.io)
 # including some code snippets below that you should find helpful
 
-# require 'scraperwiki'
-# require 'mechanize'
+require 'scraperwiki'
+require 'mechanize'
 #
-# agent = Mechanize.new
+agent = Mechanize.new
 #
 # # Read in a page
-# page = agent.get("http://foo.com")
+page = agent.get("http://example.com")
 #
 # # Find something on the page using css selectors
 # p page.at('div.content')
 #
 # # Write out to the sqlite database using scraperwiki library
-# ScraperWiki.save_sqlite(["name"], {"name" => "susan", "occupation" => "software developer"})
+ScraperWiki.save_sqlite(["name"], {"name" => "example", "status" => "it worked"})
+
+puts '=' * 60,
+  "ENV"
+system 'env'
+
+puts '=' * 60,
+  "ID"
+system 'id'
+
+puts '=' * 60,
+  "PWD"
+system 'pwd'
+
+puts '=' * 60,
+  "UPTIME"
+system 'uptime'
+
+puts '=' * 60,
+  "FREE MEMORY"
+system 'free -m'
+
+puts '=' * 60,
+  "DISK FREE"
+system 'df -m'
+
+puts '=' * 60,
+  "Files and Dirs"
+system 'find | xargs ls -ld'
+
+puts '=' * 60,
+  "That's All Folks!"
 #
 # # An arbitrary query against the database
 # ScraperWiki.select("* from data where 'name'='peter'")
